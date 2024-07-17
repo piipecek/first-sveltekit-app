dd5a2fec0f07dd4ed10f3bf2d8cdbc6a9de42fbc
Josef Lát <josef.latj@gmail.com>
2024-07-17 20:03:22 +0200

    init commit

---
+++ src/lib/title.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let title = "Welcoome to Píípstránka";
+</script>
+
+<div class="title">
+    <h1>{title}</h1>
+</div>
+
+<style>
+    .title {
+        display: flex;
+    }
+    .title h1 {
+        margin: 2rem;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Title from '$lib/title.svelte';
+</script>
+
+
+<div class="hlavni">
+    <Title />
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+</ul>
+
+
+<style>
+    .hlavni {
+        margin: 2rem;
+        font: 1rem sans-serif;
+    }
+    h1 {
+        color: #999;
+    }
+    p {
+        color: blue;
+    }
+    a {
+        color: red;
+    }
+</style>
