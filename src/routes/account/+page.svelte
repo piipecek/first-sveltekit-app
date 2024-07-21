7a9c962158ce967f043a281e5ebaa6515263e80f
Josef Lát <josef.latj@gmail.com>
2024-07-21 16:06:52 +0200

    delete users

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -3,10 +3,11 @@
     import { goto } from '$app/navigation';
     import { PUBLIC_API_URL } from "$env/static/public"
     import { addMessage } from '$lib/stores/messages.js';
+    import { onMount } from 'svelte';
 
-    
+    let users = []
 
-    let users = async () => {
+    async function get_users () {
         let response = await fetch(PUBLIC_API_URL + "/user/users", {
             method: "GET",
             headers: {
@@ -17,23 +18,40 @@
         
         if (response.ok) {
             let data = await response.json()
-            return data
+            users = data
         } else {
             addMessage("error", "Nepodařilo se načíst data.")
             goto("/login")
         }
     }
+
+    function deleteUser(id) {
+        fetch(PUBLIC_API_URL + "/user/" + id, {
+            method: "DELETE",
+            headers: {
+                "content-type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("access_token")
+            }
+        }).then(response => {
+            if (response.ok) {
+                users = users.filter(user => user.id !== id)
+                console.log(response)
+                addMessage("success", "Uživatel byl smazán.")
+            } else {
+                addMessage("error", "Nepodařilo se smazat uživatele.")
+            }
+        })
+    }
+    onMount(() => {
+        get_users()
+    })
+
 </script>
 
 <H1 text="Účet"/>
-{#await users()}
-    <p>Loading...</p>
-{:then data}
-    {#each data as user}
-        <p>{user.email}</p>
-    {/each}
-{:catch error}
-    <p>{error}</p>
-{/await}
+{#each users as user}
+    <p>{user.email}</p>
+    <button on:click={() => {deleteUser(user.id)}}>Smazat</button>
+{/each}
 
 <button on:click={() => {addMessage("success", "Test")}}>Přidat message</button>
